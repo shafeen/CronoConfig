763bdf29d58f326318627716de54203b56d84928
Shafeen M <sodl02@gmail.com>
2019-03-19 01:47:14 -0700

    added functions to create/list table folders

--- prototypes/index.js
+++ prototypes/index.js
@@ -73,7 +73,7 @@ function testSimpleGit() {
         }
     }).then(() => {
         // at this point the repo should have been initialized
-        fs.appendFile(`${FOLDERPATH}/message.txt`, "\nblah blah blah", {encoding: 'utf8'}, (err) => {
+        fs.appendFile(`${FOLDERPATH}/message.txt`, "blah blah blah\n", {encoding: 'utf8'}, (err) => {
             if (err) throw err;
             simplegit.add(`./message.txt`)
             .then(() => {
@@ -85,9 +85,58 @@ function testSimpleGit() {
     });
 }
 
+/**
+ * @param {string} tableName
+ */
+function createTable(tableName) {
+    if (!(typeof tableName === 'string' && tableName.length)) {
+        throw new Error('Invalid tableName specified');
+    }
+    const TABLE_FOLDERNAME = tableName + '.table';
+    const TABLE_PATH = `${FOLDERPATH}/${TABLE_FOLDERNAME}`;
+    const TABLECONFIG_FILENAME = '.tableconfig';
+    const TABLECONFIG_PATH = `${FOLDERPATH}/${TABLE_FOLDERNAME}/${TABLECONFIG_FILENAME}`;
+
+    const tableExists = fs.existsSync(TABLE_PATH) && fs.existsSync(TABLECONFIG_PATH);
+    if (tableExists) {
+        console.log(`Table "${tableName}" already exists.`);
+    } else if (fs.existsSync(TABLE_PATH)) {
+        console.log(`Cannot create table. Folder already exists with that name`);
+    } else {
+        fs.mkdirSync(TABLE_PATH);
+        const tableConfig = {
+            name: tableName,
+            created: new Date()
+        };
+        fs.writeFileSync(TABLECONFIG_PATH, bson.serialize(tableConfig));
+        console.log(`Created table: "${tableName}"`)
+    }
+}
+
+function listExistingTables() {
+    // table folders are currently in the format "<table_name>.table"
+    const folderContents = fs.readdirSync(FOLDERPATH, {withFileTypes: true}).filter(
+        name => {
+            return (name !== '.git') &&
+                fs.lstatSync(`${FOLDERPATH}/${name}`).isDirectory();
+        }
+    );
+    const tableFolders = folderContents.filter(name => {
+        return (name.indexOf('.table') !== -1) &&
+            fs.readdirSync(`${FOLDERPATH}/${name}`).indexOf('.tableconfig') !== -1;
+    });
+    const tableNames = tableFolders.map(folderName => folderName.split('.table')[0]);
+    console.log(tableNames);
+}
+
 // testDiff(
 //  "a: orange\nb: beep boop\nc: blah blah",
 //  "a: one\nb: beep boop\npeep boob blah\nc: blah blah"
 //  );
 // testBson();
-// testSimpleGit();
+testSimpleGit();
+['prototype_settings', 'config_settings', 'crono_settings']
+    .forEach(
+        tableName => createTable(tableName)
+    );
+listExistingTables();
